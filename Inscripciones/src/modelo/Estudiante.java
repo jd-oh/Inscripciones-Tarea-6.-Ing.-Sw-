a725739040f34dc0e9b334a0a83e74dd718d4c09
Juan David Osorio <juandavidoh3@gmail.com>
2021-11-30 17:48:04 -0500

    Creación de métodos(vacíos) y atributos que se utilizarán.

--- Inscripciones/src/modelo/Estudiante.java
+++ Inscripciones/src/modelo/Estudiante.java
@@ -5,10 +5,55 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author nicoe
+ * @author Juan David Osorio y Nicolás Estupiñán
  */
 public class Estudiante {
+    
+    private String nombre;
+    private String cedula;
+    private ArrayList<String> codigoMateriasCursadas;
+
+    public Estudiante(String nombre, String cedula, ArrayList<String> codMateriasCursadas) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.codigoMateriasCursadas = codMateriasCursadas;
+    }
+
+    public Estudiante() {
+        
+    }
+   
+    
+    
+    public ArrayList<String> mostrarCodigoMaterias(){
+        return codigoMateriasCursadas;
+    }
+
+    public void agregarCodMateriasCursadas(String codigoMateria) {
+        codigoMateriasCursadas.add(codigoMateria);
+    }
+    
+  
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
     
 }
